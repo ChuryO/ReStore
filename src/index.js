f9ac2aba6cf7aab27c7606bb02db268f4fbdb9d5
Oleksandr Churychev <churychev.oleksandr@gmail.com>
2020-03-26 21:04:09 +0100

    create basic project structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from "./components/app";
+
+// Bootstrap
+import './bootstrap.min.css';
+
+
+ReactDom.render(<App/>, document.getElementById('root'));
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,5 @@
+export default class Bookstore {
+    getBooks() {
+        return [];
+    }
+};
