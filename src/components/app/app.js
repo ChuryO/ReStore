8b8ac0d2b4de559b4e9cf4db7a493370cdaa912f
Oleksandr Churychev <churychev.oleksandr@gmail.com>
2020-03-30 19:46:46 +0200

    add basic style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-import './bootstrap.min.css';
 import './style.css';
-import {CartPage, HomePage} from '../pages';
+
+import { CartPage, HomePage } from '../pages';
+import Header from '../header';
 
 const App = () => {
-    return (
-        <div className='container'>
-            <h2>Hello From Header!!</h2>
-            <Switch>
-                <Route path='/' component={HomePage} exact/>
-                <Route path='/cart/' component={CartPage}/>
-                {/*<Route render={() => <h3>Page not found</h3>}/>*/}
-            </Switch>
-        </div>
-    );
+	return (
+		<main role='main' className='container'>
+			<Header numItems={3} total={136}/>
+			<Switch>
+				<Route path='/' component={ HomePage } exact/>
+				<Route path='/cart/' component={ CartPage }/>
+				<Route render={() => <h3>Page not found</h3>}/>
+			</Switch>
+		</main>
+	);
 };
 
 export default App;
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,12 +1,20 @@
 export default class BookstoreService {
-    getBooks() {
-        return [
-            {id: 1, title: 'Pride and Prejudice (Oxford World’s Classics)', author: 'Some Author'},
-            {id: 2, title: 'The Lord of the Rings', author: 'Some Author'},
-            {id: 3, title: 'Middlemarch (Penguin Classics)', author: 'Some Author'},
-            {id: 4, title: 'To Kill a Mockingbird', author: 'Some Author'},
-            {id: 5, title: 'Midnight’s Children: A Novel', author: 'Some Author'},
-            {id: 6, title: 'Crime and Punishment (Penguin Classics)', author: 'Some Author'},
-        ];
-    }
+	getBooks() {
+		return [
+			{
+				id: 1,
+				title: "Pride and Prejudice (Oxford World’s Classics)",
+				author: "Some Author",
+				price: 10,
+				coverImage: "https://source.unsplash.com/120x180/?book"
+			},
+			{
+				id: 2,
+				title: "The Lord of the Rings",
+				author: "Some Author 2",
+				price: 134,
+				coverImage: "https://source.unsplash.com/120x180/?books"
+			},
+		];
+	}
 };
