7604ee0d33b372eba8aa7fefdcf874cbb55d77e1
Oleksandr Churychev <churychev.oleksandr@gmail.com>
2020-04-07 08:16:00 +0200

    Store enhancer

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,20 +1,30 @@
 export default class BookstoreService {
+	data = [
+		{
+			id: 1,
+			title: "Pride and Prejudice (Oxford World’s Classics)",
+			author: "Some Author",
+			price: 10,
+			coverImage: 'https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&h=150&w=200'
+		},
+		{
+			id: 2,
+			title: "The Lord of the Rings",
+			author: "Some Author 2",
+			price: 134,
+			coverImage: 'https://images.pexels.com/photos/635017/pexels-photo-635017.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=150&w=250'
+		},
+	];
+
 	getBooks() {
-		return [
-			{
-				id: 1,
-				title: "Pride and Prejudice (Oxford World’s Classics)",
-				author: "Some Author",
-				price: 10,
-				coverImage: "https://source.unsplash.com/120x180/?book"
-			},
-			{
-				id: 2,
-				title: "The Lord of the Rings",
-				author: "Some Author 2",
-				price: 134,
-				coverImage: "https://source.unsplash.com/120x180/?books"
-			},
-		];
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (Math.random() > .75) {
+					reject(new Error('Error in bookstore service'));
+				} else {
+					resolve(this.data);
+				}
+			}, 700);
+		});
 	}
 };
--- src/store.js
+++ src/store.js
@@ -1,6 +1,47 @@
-import {createStore} from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers/reducer';
 
-const store = createStore(reducer);
+
+
+// Store Enhance
+const stringEnhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+	const originalDispatch = store.dispatch;
+	store.dispatch = (action) => {
+		if (typeof action === 'string') {
+			return originalDispatch({
+				type: action
+			});
+		}
+		return originalDispatch(action);
+	};
+	return store;
+};
+
+// Store Enhance
+const logEnhancer = (createStore) => (...args) => {
+	const store = createStore(...args);
+	const originalDispatch = store.dispatch;
+	store.dispatch = (action) => {
+		console.log(action);
+		return originalDispatch(action);
+	};
+	return store;
+};
+
+// // Monkey patching
+// const originalDispatch = store.dispatch;
+// store.dispatch = (action) =>{
+// 	if (typeof action === 'string'){
+// 		return originalDispatch({
+// 			type: action
+// 		})
+// 	}
+// 	return originalDispatch(action)
+// };
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
